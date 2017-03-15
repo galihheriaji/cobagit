490b63e2c11460bf4c90c31153314ac2b31dda38
Galih <galihwimba@gmail.com>
2017-03-15 10:55:09 +0700

    firstgit

---
+++ app/src/main/java/com/example/galih/git_galih/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.galih.git_galih;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        x = 5;
+        y = 6;
+    }
+}
